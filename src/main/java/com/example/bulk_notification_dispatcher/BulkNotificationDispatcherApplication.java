4dfa370105a50a3663b16277ca9aeed2e748eca0
Sangeetha Racha <sangeetharacha@Sangeethas-MacBook-Pro.local>
2025-12-19 10:36:59 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/bulk_notification_dispatcher/BulkNotificationDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulk_notification_dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkNotificationDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkNotificationDispatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bulk_notification_dispatcher/BulkNotificationDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bulk_notification_dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkNotificationDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
